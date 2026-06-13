9951c3c99c0e10cd41aacc59a98b77fde1f022dc
sasepa-mx <sasepamx@gmail.com>
2026-06-13 16:05:00 -0600

    Delete js directory

--- js/mapbox-config.js
+++
@@ -1,2 +0,0 @@
-// ©️ SASEPAMx Derechos Reservados Prohibida su copia total o parcial sin autorización expresa
-window.MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiZGVmcmVnZXN3IiwiYSI6ImNsZjRxcGl2NzB2MXozcm80MGI4emhmY2kifQ.ebt3Xjo6bnAw4E09k9gtLQ';
